3e7bcbddeef5f30c5cac03ba9ff34243ea1888cc
Vladislav Kurenkov <castielblak@gmail.com>
2019-03-19 19:13:08 +0300

    Add automatic replay conversion to binary format

---
+++ converter.py
@@ -0,0 +1,24 @@
+import os
+import argparse
+
+def convert_to_bin(path: str):
+    return os.system("nodejs connector/replay.js convert {} {}".format(path, path + ".bin"))
+def convert_all(path: str):
+    num_all = 0
+    num_suc = 0
+    for subdir, dirs, files in os.walk(path):
+        for file in files:
+            code = convert_to_bin(os.path.join(subdir, file))
+            if code == 0:
+                num_suc += 1
+            num_all += 1
+            
+    print("Total files: {}; Success: {} ({})".format(num_all, num_suc, num_suc/num_all))
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--path", type=str, help="Folder with replays must be put here.")
+args   = parser.parse_args()
+
+print("Starting to convert...")
+convert_all(args.path)
+print("Done.")
