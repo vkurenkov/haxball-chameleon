db2e27946958e2abe388f1b01e1fa1cf6458a45b
Vladislav Kurenkov <castielblak@gmail.com>
2019-04-21 21:20:17 +0300

    make some beauty

--- run_bot.py
+++ run_bot.py
@@ -2,13 +2,12 @@ import argparse
 import haxball.async_common as async_common
 
 from bots.bot_example import ChasingBot
-from bots.lgbm.bot    import LGBMBot
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
 	parser.add_argument("--channel", type=int, default=0, help="Different agents must be placed on different communication channels.")
 	args = parser.parse_args()
 	try:
-		async_common.run(LGBMBot(str(args.channel)).play())
+		async_common.run(ChasingBot(str(args.channel)).play())
 	except KeyboardInterrupt:
 		pass
